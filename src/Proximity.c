47aa4c89d14ae87058e48ab3f1d2f23a4e71eb7c
Greek Hallstrom <Greek Hallstrom>
2016-04-30 16:41:07 -0600

    Commented prox and servo

--- src/Proximity.c
+++ src/Proximity.c
@@ -8,6 +8,9 @@ uint32_t UltraSonic_Threshold = 15;
 osThreadId proximityTaskHandle;
 uint32_t UltraSonic_Enabled = 1;
 
+/*
+ * Start the ADC in continuous mode and enable the TIM15 Input Capture interupt.
+ */
 void Proximity_Init(void)
 {
   spinning = 0;
@@ -22,15 +25,22 @@ void Proximity_Task(void const * argument)
 
   for(;;)
   {
+	  /*
+	   * Loop forever and keep reading ADC value (IR censor distance)
+	   */
     if(HAL_ADC_PollForConversion(&hadc1, 1000) == HAL_OK)
     {
       adcValue = HAL_ADC_GetValue(&hadc1);
+      /*
+       * If the value in above a threshold (close enough to a wall) we fire the ultrasonic
+       */
       if(adcValue > ADC_Threshold && UltraSonic_Enabled && !spinning)
       {
         UltraSonic_Enabled = 0;
-        PIDController_Stop();
-        osDelay(1000);
+        PIDController_Stop();	//Stop once we've seen a wall
+        osDelay(1000);			//Wait a second for robot to come to a complete stop
 
+        //Switch the ultrasonic pin to an output so we can send a trigger pulse.
         GPIO_InitTypeDef GPIO_InitStruct;
         GPIO_InitStruct.Pin = Ultrasonic_1_Echo_Pin;
         GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
@@ -38,13 +48,17 @@ void Proximity_Task(void const * argument)
         GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
         HAL_GPIO_Init(Ultrasonic_1_Echo_GPIO_Port, &GPIO_InitStruct);
 
+        //Send a trigger pulse of 1ms
         HAL_GPIO_WritePin(Ultrasonic_1_Echo_GPIO_Port, Ultrasonic_1_Echo_Pin, GPIO_PIN_SET);
         osDelay(1);
         HAL_GPIO_WritePin(Ultrasonic_1_Echo_GPIO_Port, Ultrasonic_1_Echo_Pin, GPIO_PIN_RESET);
 
+        //Set pin back to input capture mode to read the echo back from the ultrasonic
         HAL_TIM_Base_MspInit(&htim15);
 
       }
+      //ADC is disabled after it senses a wall, until the robot stops spinning and
+      //a wall isn't in front of the robot anymore
       else if(adcValue < (ADC_Threshold - 300) && !spinning)
       {
         UltraSonic_Enabled = 1;
@@ -55,12 +69,18 @@ void Proximity_Task(void const * argument)
   }
 }
 
+/*
+ * Regester the task for FreeRTOS
+ */
 void Proximity_Register(void)
 {
   osThreadDef(proximityTask, Proximity_Task, osPriorityNormal, 0, configMINIMAL_STACK_SIZE);
   proximityTaskHandle = osThreadCreate(osThread(proximityTask), NULL);
 }
 
+/*
+ * Read ultrasonic echo value and determine if we have a stair or a wall
+ */
 void ProcessUltrasonic(TIM_HandleTypeDef *htim)
 {
   volatile uint32_t captureValue = HAL_TIM_ReadCapturedValue(htim, TIM_CHANNEL_1);
@@ -68,10 +88,11 @@ void ProcessUltrasonic(TIM_HandleTypeDef *htim)
   // found a stair
   if(captureValue > UltraSonic_Threshold)
   {
-    Servo_SetPosition(75);
-    PIDController_Start();
-    spinning = 1;
-    return;
+    Servo_SetPosition(75);	//Put out arm and
+    PIDController_Start();	//start climbing
+    spinning = 1;			//Disables certain actions while in spin state
+    return;					//spinning is confusingly named, as it is any state
+    						//where robot isn't looking for an obstacle in front of it.
   }
 
   // Tell the PID Controller to execute a spin
--- src/Servo.c
+++ src/Servo.c
@@ -11,6 +11,9 @@ uint32_t counter;
 uint16_t scaledPosition;
 uint32_t count;
 
+/*
+ * Initiate variables and start timer with interupt
+ */
 void Servo_Init()
 {
   counter = 0;
@@ -20,41 +23,48 @@ void Servo_Init()
   HAL_TIM_Base_Start_IT(&htim7);
 }
 
+/*
+ * Register the servo task with FreeRTOS
+ */
 void Servo_Register()
 {
   osThreadDef(servoTask, Servo_Task, osPriorityNormal, 0, configMINIMAL_STACK_SIZE);
   Servo_TaskHandle = osThreadCreate(osThread(servoTask), NULL);
 }
 
+/*
+ * Maps a value from 0-100 to 5-25. A 5ms pulse retracts arms and a 25ms pulse extends arms
+ */
 uint16_t map(uint16_t x, uint16_t in_min, uint16_t in_max, uint16_t out_min, uint16_t out_max)
 {
   return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
 
-/**
- * Position should be between 0 - 100
- * 50 indicates the servos natural position
- */
+
 void Servo_SetPosition(uint8_t position)
 {
   // Map the position to a valid percentage
   scaledPosition = map(position, 0, 100, 5, 25);
 }
 
+/*
+ * Dummy task. All the work is done in the timer interupt.
+ */
 void Servo_Task()
 {
   // Set the servo to it's natural position
-  // this should generate a pulse width of 1.5ms
   Servo_SetPosition(0);
 
   while(1)
   {
-    // servoPosition += 50;
-    // __HAL_TIM_SET_COMPARE(&htim16, TIM_CHANNEL_1, servoPosition);
     osDelay(1000);
   }
 }
 
+/*
+ * Called from timer interupt. Generates a PWM signal for the servo depending on
+ * the value in scaledPosition
+ */
 void Handle_PWM(void)
 {
   counter++;
